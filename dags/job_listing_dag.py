46f0e352c9e92f87907e8f0a0e8f702a847797f9
joe <joeip0411@gmail.com>
2024-04-15 22:39:34 +1000

    open ai chat completion

--- dags/job_listing_dag.py
+++ dags/job_listing_dag.py
@@ -11,8 +11,9 @@ from airflow.decorators import dag, task
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.http.hooks.http import HttpHook
 from bs4 import BeautifulSoup
+from openai import OpenAI
 
-ADZUNA_SEARCH_ENDPOINT = "https://api.adzuna.com/v1/api/jobs/au/search/{page_number}?app_id={app_id}&app_key={app_key}&what={search_content}&results_per_page=50"
+ADZUNA_SEARCH_ENDPOINT = "https://api.adzuna.com/v1/api/jobs/au/search/{page_number}?app_id={app_id}&app_key={app_key}&what={search_content}&results_per_page=10"
 BUCKET_NAME = 'joeip-data-engineering-job-listing'
 class AdzunaHook(HttpHook):
     """Interacts with Adzuna job listing API
@@ -52,10 +53,55 @@ class AdzunaHook(HttpHook):
             if len(result) > 0:
                 all_result += result
                 page_number += 1
+                break
             else:
                 break
 
         return all_result
+
+class ChatCompletionHook(HttpHook):
+    """Interacts with OpenAI Chat Completion API
+    api secret in "openai_conn"
+    """
+        
+    def __init__(self, conn_id:str):
+        """Open AI Chat Completion Hook
+
+        Args:
+            conn_id (str): Airflow connection id to obtain api secret
+        """
+        self.conn_id = conn_id
+        self.connection = self.get_connection(self.conn_id)
+        self.api_key = self.connection.password
+
+    def get_technologies_from_job_desc(self, job_desc:str) -> str:
+        """Extract technologies required from job description
+
+        Args:
+            job_desc (str): job description
+
+        Returns:
+            str: technologies delimited by " | "
+        """
+        client = OpenAI(api_key=self.api_key)
+
+        response = client.chat.completions.create(
+            model='gpt-3.5-turbo',
+            messages = [
+                {"role": "system", "content": "You are a helpful technology assistant who is aware of all cloud services and technology used for software development and data processing"},
+                {"role": "user", "content": "Find all the cloud services and technology required for the job descripton provided"},
+                {"role": "user", "content": "If the technology / cloud service identified is not the official name of the product, return the official name"},
+                {"role": "user", "content": "Remove duplicates if there are any"},
+                {"role": "user", "content": "Return 'N/A' if none identified"},
+                {"role": "user", "content": "Return the result delimited by ' | '"},
+                {"role": "user", "content": "Job description: \n" + job_desc},
+            ],
+            temperature=0.2,
+
+        )
+        content = response.choices[0].message.content
+        return content
+
     
 @dag(dag_id='job_listing_processing',
      start_date=pendulum.datetime(2024,4,8, tz='UTC'),
@@ -138,7 +184,29 @@ def job_listing_processing():
         obj = io.BytesIO(s3_obj.get()['Body'].read())
         return obj
     
+    @task
+    def get_technologies_from_job_description(key:str):
+        job_desc = pd.read_parquet(_get_obj_from_s3(key=key)) 
+
+        technologies = []
+        chat_completion_hook = ChatCompletionHook(conn_id='openai_conn')
+
+        for i in range(len(job_desc)):
+            technology = chat_completion_hook.get_technologies_from_job_desc(job_desc=job_desc.iloc[i,1])
+            technologies.append(technology)
+
+        job_desc['technology'] = technologies
+
+        file_name = 'job_descriptions_with_skill.parquet'
+        output_key = datetime.utcnow().strftime('raw/%Y/%m/%d/')+ file_name
+        _load_obj_to_s3(file_name=file_name, key=output_key, obj=job_desc)
+
+        return output_key
+
+
     job_listings = get_job_listing(search_content='data%20engineer')
-    get_job_descriptions(job_listings)
+    job_descriptions = get_job_descriptions(job_listings)
+    get_technologies_from_job_description(job_descriptions)
+
 
 job_listing_processing()
